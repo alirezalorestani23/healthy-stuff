525e1e8d858e5bf6828b95898cb083d5e1d60cfc
alireza.lorestani <alirezalorestani2010@gmail.com>
2026-02-05 01:45:19 -0500

    -added BottomNavBar and all 4 screens to that

--- app/src/main/java/com/example/healthystuff/presentation/navigation/NavGraph.kt
+++ app/src/main/java/com/example/healthystuff/presentation/navigation/NavGraph.kt
@@ -1,8 +1,12 @@
 package com.example.healthystuff.presentation.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.healthystuff.presentation.screen.dashboard.DashboardScreen
 import com.example.healthystuff.presentation.screen.food.FoodLogScreen
@@ -12,11 +16,25 @@ import com.example.healthystuff.presentation.screen.workout.WorkoutLogScreen
 @Composable
 fun NavGraph(startDestination: String = Screen.Dashboard.route) {
     val navController = rememberNavController()
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = backStackEntry.value?.destination?.route
 
-    NavHost(navController = navController, startDestination = startDestination) {
-        composable(Screen.Dashboard.route) { DashboardScreen() }
-        composable(Screen.FoodLog.route) { FoodLogScreen() }
-        composable(Screen.WorkoutLog.route) { WorkoutLogScreen() }
-        composable(Screen.Profile.route) { ProfileScreen() }
+    Scaffold(bottomBar = {
+        BottomNavBar(
+            navController = navController,
+            currentRoute = currentRoute
+        )
+    }
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = startDestination,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Dashboard.route) { DashboardScreen() }
+            composable(Screen.FoodLog.route) { FoodLogScreen() }
+            composable(Screen.WorkoutLog.route) { WorkoutLogScreen() }
+            composable(Screen.Profile.route) { ProfileScreen() }
+        }
     }
 }
